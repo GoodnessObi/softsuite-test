e39669792d54ea724209f8a8d6600c28907fbb16
GoodnessObi <goodnessobi@gmail.com>
2024-01-06 12:29:29 +0100

    ui: style empty state

--- src/components/base/Emptystate/EmptyState.tsx
+++ src/components/base/Emptystate/EmptyState.tsx
@@ -1,10 +1,11 @@
-import Icons from "../../../assets/images";
+import Icons from '../../../assets/images';
+import './EmptyState.scss';
 
-export default function EmptyState () {
-  return (
-    <div className="empty">
-      <img src={Icons['Empty']} alt="" />
-      <p>There are no elements to display</p>
-    </div>
-  )
+export default function EmptyState() {
+	return (
+		<div className='empty'>
+			<img src={Icons['Empty']} alt='' />
+			<p>There are no elements to display</p>
+		</div>
+	);
 }
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router-dom';
-// import './DashboardLayout.scss';
+import './Layout.scss';
 import SideNav from './components/sidenav/SideNav';
 import TopNav from './components/topnav/TopNav';
 import { useState } from 'react';
 // import { useState } from 'react';
-// 
+//
 export function PageLayout() {
 	const [navIsOpen, setIsOpen] = useState<boolean>(false);
 
