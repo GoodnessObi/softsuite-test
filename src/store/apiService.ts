187edf8f8a8303452e91af1a84066293dd66fc8c
GoodnessObi <goodnessobi@gmail.com>
2024-01-08 02:21:13 +0100

    feat: define all api routes

--- src/store/apiService.ts
+++ src/store/apiService.ts
@@ -1,18 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { Element } from '../types/apiResponseTypes';
+import {
+	Element,
+	ElementLink,
+	Grade,
+	Suborganization,
+} from '../types/apiResponseTypes';
+
+type ElementLinkIds = {
+	id: string;
+	elementId: string;
+};
 
 export const apiService = createApi({
 	reducerPath: 'apiService',
 	baseQuery: fetchBaseQuery({
 		baseUrl: 'https://650af6bedfd73d1fab094cf7.mockapi.io',
 	}),
-	tagTypes: ['elements'],
+	tagTypes: ['elements', 'elementLinks'],
 	endpoints: (builder) => ({
 		getElements: builder.query<Element[], void>({
 			query: () => ({ url: 'elements' }),
 			transformResponse: (response: { data: { content: Element[] } }) => {
 				return response.data.content;
 			},
+			providesTags: ['elements'],
 		}),
 		getElement: builder.query<Element, string>({
 			query: (id) => ({ url: `elements/${id}` }),
@@ -42,20 +53,95 @@ export const apiService = createApi({
 			invalidatesTags: ['elements'],
 		}),
 
-		// getPet: builder.query<Pet, string>({
-		//   query: (id) => ({ url: "pets", params: { id } }),
-		//   transformResponse: (response) => response.pets[0],
+		//Elementlinks
+		getElementLinks: builder.query<ElementLink[], string>({
+			query: (path) => `elements/${path}/elementlinks`,
+			transformResponse: (response: { data: { content: ElementLink[] } }) => {
+				return response.data.content;
+			},
+			providesTags: ['elementLinks'],
+		}),
+		getElementLink: builder.query<ElementLink, ElementLinkIds>({
+			query: (value) => `elements/${value.elementId}/elementlinks/${value.id}`,
+		}),
+		addElementLink: builder.mutation<ElementLink, ElementLink>({
+			query: (data) => ({
+				url: `elements/${data.elementId}/elementlinks`,
+				method: 'POST',
+				body: data,
+			}),
+			invalidatesTags: ['elementLinks'],
+		}),
+		updateElementLink: builder.mutation<ElementLink, ElementLink>({
+			query: (data) => ({
+				url: `elements/${data.elementId}/elementlinks/${data.id}`,
+				method: 'PUT',
+				body: data,
+			}),
+			invalidatesTags: ['elementLinks'],
+		}),
+		deleteElementLink: builder.mutation<void, ElementLinkIds>({
+			query: (data) => ({
+				url: `elements/${data.elementId}/elementlinks/${data.id}`,
+				method: 'DELETE',
+			}),
+			invalidatesTags: ['elementLinks'],
+		}),
+
+		getGrades: builder.query({
+			query: () => 'grade',
+			transformResponse: (response: { data: Grade[] }) => {
+				return response.data;
+			},
+		}),
+		getGradeSteps: builder.query({
+			query: (id) => `grade/${id}/gradesteps`,
+		}),
+		getSuborganizations: builder.query({
+			query: () => 'suborganizations',
+			transformResponse: (response: { data: Suborganization }) => {
+				return response.data;
+			},
+		}),
+		getDepartments: builder.query({
+			query: (id) => `suborganizations/${id}/departments`,
+		}),
+
+		// Include endpoints from lookupApi
+		getLookupValues: builder.query({
+			query: (id) => `lookups/${id}/lookupvalues`,
+		}),
+		// fetchElementClassification: builder.query({
+		// 	query: () => 'lookups/2/lookupvalues',
 		// }),
-		// getBreeds: builder.query<Pet[], Animal>({
-		//   query: (animal) => ({ url: "breeds", params: { animal } }),
-		//   transformResponse: (response) => response.breeds,
+		// fetchPayrun: builder.query({
+		// 	query: () => 'lookups/5/lookupvalues',
 		// }),
-		// search: builder.query<Pet[], searchState>({
-		//   query: ({ animal, location, breed }) => ({
-		//     url: "pets",
-		//     params: { animal, location, breed },
-		//   }),
-		//   transformResponse: (response) => response.pets,
+
+		// fetchJobTitle: builder.query({
+		// 	query: () => 'lookups/6/lookupvalues',
+		// }),
+		// fetchLocation: builder.query({
+		// 	query: () => 'lookups/7/lookupvalues',
+		// }),
+		// fetchEmployeeType: builder.query({
+		// 	query: () => 'lookups/4/lookupvalues',
+		// }),
+		// fetchEmployeeCategory: builder.query({
+		// 	query: () => 'lookups/5/lookupvalues',
+		// }),
+
+		// fetchUnion: builder.query({
+		// 	query: () => 'lookups/8/lookupvalues',
+		// }),
+		// fetchHousing: builder.query({
+		// 	query: () => 'lookups/9/lookupvalues',
+		// }),
+		// fetchWardrobe: builder.query({
+		// 	query: () => 'lookups/10/lookupvalues',
+		// }),
+		// fetchSecurity: builder.query({
+		// 	query: () => 'lookups/11/lookupvalues',
 		// }),
 	}),
 });
@@ -66,4 +152,20 @@ export const {
 	useCreateElementMutation,
 	useUpdateElementMutation,
 	useDeleteElementMutation,
+
+	//elementlinks
+	useGetElementLinksQuery,
+	useGetElementLinkQuery,
+	useAddElementLinkMutation,
+	useUpdateElementLinkMutation,
+	useDeleteElementLinkMutation,
+
+	//grades
+	useGetGradesQuery,
+	useGetGradeStepsQuery,
+	useGetSuborganizationsQuery,
+	useGetDepartmentsQuery,
+
+	//lookup
+	useGetLookupValuesQuery,
 } = apiService;
