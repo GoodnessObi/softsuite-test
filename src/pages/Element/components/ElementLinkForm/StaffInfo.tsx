5912c15d466ab7eed38fbf8bb9d23cf4dc0bd781
GoodnessObi <goodnessobi@gmail.com>
2024-01-09 00:05:19 +0100

    feat: create staff infoform

--- src/pages/Element/components/ElementLinkForm/StaffInfo.tsx
+++ src/pages/Element/components/ElementLinkForm/StaffInfo.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useEffect } from 'react';
-import { Input, SelectBox, TextArea } from '../../../../components/base/Form';
+import { Input, SelectBox } from '../../../../components/base/Form';
 import Button from '../../../../components/base/Button/Button';
 import { linkFormSteps } from '../../../../lib/data';
 import { FormElementLinkType } from '../../../../types/apiResponseTypes';
