bd28a1c1cc74a7e344eeb517adb31e033faf33d9
GoodnessObi <goodnessobi@gmail.com>
2024-01-11 00:39:59 +0100

    ui: add page router

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,10 +3,12 @@ import './Layout.scss';
 import SideNav from './components/sidenav/SideNav';
 import TopNav from './components/topnav/TopNav';
 import { useState } from 'react';
-// import { useState } from 'react';
+import PageRouter from '../components/base/PageRouter/PageRouter';
+import { useAppSelector } from '../store/hook';
 //
 export function PageLayout() {
 	const [navIsOpen, setIsOpen] = useState<boolean>(false);
+	const routes = useAppSelector((state) => state.baseState.routes);
 
 	const toggleNav = () => {
 		setIsOpen((prev) => !prev);
@@ -17,6 +19,7 @@ export function PageLayout() {
 			<TopNav toggleNav={toggleNav} navIsOpen={navIsOpen} />
 			<SideNav navIsOpen={navIsOpen} />
 			<div className='layout__body'>
+				<PageRouter pages={routes} />
 				<Outlet />
 			</div>
 		</div>
